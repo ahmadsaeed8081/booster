630aab71b68b8e94954c8abde959caffb61ebee0
ahmad <ahmadsaeed8081@gmail.com>
2024-10-30 07:47:03 +0500

    ten.3 commit

--- src/configs/Contracts.js
+++ src/configs/Contracts.js
@@ -1,6 +1,6 @@
 
 export const usdt_address="0x341343568948459e5b7017eDDb05110cfA3EF699";
-export const cont_address="0x555be66E82c220E2608fF05CDcCd9A388C13958E";
+export const cont_address="0xa328F1d8064f362b66d6d0629333a4d39585579c";
 export const cont_Name="0x6f922a3d0DeC230BF3595b9c744403aD6F9a0CC9";
 
 export const cont_Name_abi=[
